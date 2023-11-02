82803b46f8f818f9aeca0cedf0f9ae95a0547b40
Christian <c.espinoza.code@gmail.com>
2023-11-02 17:32:50 -0500

    componente asincrono implementeado en la app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,39 @@
 <script setup lang="ts">
-import Home from '@/components/Home.vue';
+// import Home from '@/components/Home.vue';
+import SplashScreen from './components/SplashScreen.vue';
+import { defineAsyncComponent } from 'vue';
+
+const Home = defineAsyncComponent(async () => {
+  await new Promise((resolve) => {
+    setTimeout(resolve, 3000);
+  });
+
+  // Luego de 3 segundos, cargamos el componente de inicio
+  return import('./components/Home.vue');
+});
 </script>
 
 <template>
-  <Home />
+  <Suspense>
+    <template #default>
+      <Home />
+    </template>
+    <template #fallback>
+      <SplashScreen />
+    </template>
+  </Suspense>
 </template>
 
-<style scoped></style>
+<style scoped>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
+</style>
