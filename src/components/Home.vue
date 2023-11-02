4c51668c53837fa86fc6e5dd73ceebb9abad2ef6
Christian <c.espinoza.code@gmail.com>
2023-11-02 18:15:31 -0500

    elements moved to layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,22 @@
-<script setup lang='ts'>
-
+<script setup lang="ts">
+import HeaderComponent from './Header.vue';
+import Layout from './Layout.vue';
+import Resume from './Resume/index.vue';
+import Movements from './Movements.vue'
 </script>
 
 <template>
-  <div>
-    Cleaned
-  </div>
+  <Layout>
+    <template #header>
+      <HeaderComponent />
+    </template>
+    <template #resume>
+      <Resume />
+    </template>
+    <template #movements>
+      <Movements />
+    </template>
+  </Layout>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
