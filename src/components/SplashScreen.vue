c10e03630c3d1e6d02b8fd313bdce07b215beb7f
Christian <c.espinoza.code@gmail.com>
2023-11-02 16:26:20 -0500

    styles for splashscreen agregado

---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,25 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<style scoped>
+.splashscreen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+}
+h1 {
+  color: var(--brand-blue);
+}
+h1 span {
+  color: var(--brand-green);
+}
+</style>
