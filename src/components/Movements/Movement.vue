605d7e612fbb2f9729ab942beb3e557d8b38e32d
Christian <c.espinoza.code@gmail.com>
2023-11-03 22:26:21 -0500

    individual movement component created

---
+++ src/components/Movements/Movement.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { defineProps, toRefs } from 'vue';
+
+const props = defineProps({
+  title: {
+    type: String,
+  },
+});
+
+const { title } = toRefs(props);
+</script>
+
+<template>
+  <div class="movement">{{ title }}</div>
+</template>
+
+<style scoped></style>
