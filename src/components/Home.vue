a627ac11faebba5dd957dd714a70f30435d8932c
Christian <c.espinoza.code@gmail.com>
2023-11-03 22:05:04 -0500

    directives added to resume in Home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,12 @@
 import HeaderComponent from './Header.vue';
 import Layout from './Layout.vue';
 import Resume from './Resume/index.vue';
-import Movements from './Movements.vue'
+import Movements from './Movements.vue';
+import { ref } from 'vue';
+
+const amount = ref<number | undefined>(undefined)
+
+amount.value = 1000
 </script>
 
 <template>
@@ -11,7 +16,10 @@ import Movements from './Movements.vue'
       <HeaderComponent />
     </template>
     <template #resume>
-      <Resume />
+      <Resume :label="'04/11/2022'" :total-label="'Ahorro total'" :amount="amount" :total-amount="50000">
+        <template #graphic>graphic</template>
+        <template #action>action</template>
+      </Resume>
     </template>
     <template #movements>
       <Movements />
