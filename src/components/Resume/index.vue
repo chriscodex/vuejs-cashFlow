b82d5c5a32fee59784f48d58bb13b8544a9aa3d4
Christian <c.espinoza.code@gmail.com>
2023-11-03 19:24:16 -0500

    label & amount added to template

--- src/components/Resume/index.vue
+++ src/components/Resume/index.vue
@@ -1,13 +1,48 @@
-<script setup lang='ts'>
+<script setup lang="ts">
+import { defineProps } from 'vue';
 
+const props = defineProps({
+  label: String,
+  amount: Number,
+});
+
+const { label, amount } = props;
 </script>
 
 <template>
   <main>
-    Resume
+    <p>
+      {{ label }}
+    </p>
+    <h1>
+      {{ amount }}
+    </h1>
   </main>
 </template>
 
 <style scoped>
-
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+}
+h1,
+p {
+  margin: 0;
+  text-align: center;
+}
+h1 {
+  margin-top: 14px;
+  color: var(--brand-green);
+}
+.graphic {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 48px 24px;
+  box-sizing: border-box;
+}
 </style>
